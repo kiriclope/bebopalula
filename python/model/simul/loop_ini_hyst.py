345ba6c379a06ce0c9b05178b4981f7ef930f086
leon <kiriclope@gmail.com>
2022-06-20 18:25:25 +0200

    20-06-2022_18:25

--- python/model/simul/loop_ini_hyst.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from write import *
-
-path = os.getcwd()
-
-for i_ini in range(10):
-    replace_global('INI_COND_ID', i_ini) 
-    exec(open( path + '/hysteresis.py').read()) 
--- python/model/simul/options.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing 
-from . import constants as gv 
-
-def set_globals(**opts):
-
-    
-def set_options(**kwargs):     
-    opts = dict()
-    
